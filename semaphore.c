a31a044eb3a8c81e70502ee05e1e5c70b1c926a6
Mehzabin Haque <74411032+mehzabin-haque@users.noreply.github.com>
2023-07-30 13:11:30 +0600

    Update semaphore.c

--- semaphore.c
+++ semaphore.c
@@ -1,38 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
+#include<time.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <pthread.h>
-#include <semaphore.h>
+int mat[3][100000],count;
+pthread_t thr[3];
+sem_t lock;
 
-sem_t s;
+void* countNum(void* num){
+    int r = *(int*)num;
 
-void *routine(void *x){
+    sem_wait(&lock);
 
-    sem_wait(&s);
-    sleep(1);
-    printf("Let's Dance........... %d\n", *(int*)x);
-    sem_post(&s);
+	for(int i=0;i<100000;i++){
+		if(mat[r][i] == 1){
+		       	count++;	
+		}
+	}
+    
+    sem_post(&lock);
 }
 
 int main(){
 
-    pthread_t t;
-    int num = 10;
-    pthread_t threads[num + 1];
-
-    sem_init(&s, 0, 3);
-
-    for(int i=0; i<num; i++){
-        int *t = malloc(sizeof(int));
-        *t = i;
-        pthread_create(&threads[i], NULL, routine, (int*)t);
+    for(int i=0;i<3;i++){
+        for(int j=0;j<100000;j++){
+            mat[i][j] = 1;
+        }
     }
 
-    for(int i=0; i<num; i++){
-        pthread_join(threads[i], NULL);
+    sem_init(&lock, 0, 1);
+
+    clock_t begin_time = clock();
+    for(int i=0;i<3;i++){
+        int* s = (int*)malloc(sizeof(int));
+        *s = i;
+		pthread_create(&thr[i], NULL, countNum, (void*)s);
     }
 
-    sem_destroy(&s);
+    for(int i=0;i<3;i++){
+        pthread_join(thr[i],NULL);
+    }
 
+    clock_t end_time = clock();
+
+    printf("Num of 1's (Synchronization) : %d Time = %.2lf \n",count, (double)(end_time-begin_time)/CLOCKS_PER_SEC);
 }
